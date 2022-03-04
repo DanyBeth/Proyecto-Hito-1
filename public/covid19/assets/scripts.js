8d4bd586d10bb6bbf358a76430e8692f9c996738
Daniela Hernandez <hg.danielabelen@gmail.com>
2022-03-04 20:48:03 -0300

    Casi listo
    Sólo falta activar los modales

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,4 +1,4 @@
-
+var chart2;
 
 const getTotal = async (jwt) => {
   try {
@@ -12,13 +12,25 @@ const getTotal = async (jwt) => {
       },
     });
     const { data } = await response.json();
+    let ubicacion = []
+    let confirmados = []
+    let muertes = []
+    let recuperados = []
+    let activos = []
+    const ctx = document.getElementById('Chart')
     
-    console.log(data);
-    data.forEach(element => {
+    data.forEach((element, i) => {
       table(element.location, element.confirmed, element.deaths);
-      const ctx = document.querySelector('#chart').getContext('2d')
-      totalCasesChart(ctx, element.location, element.confirmed, element.deaths)
+      if (i<10) {
+        ubicacion.push(element.location);
+        confirmados.push(element.confirmed);
+        muertes.push(element.deaths);
+        recuperados.push(Math.floor((element.confirmed - element.deaths) * 0.7));
+        activos.push(Math.floor((element.confirmed - element.deaths) * 0.3));
+      }
     })
+    totalCasesChart(ctx, ubicacion, confirmados, muertes, recuperados, activos);
+
   } catch (err) {
     console.error(`Error: ${err}`);
   }
@@ -26,12 +38,35 @@ const getTotal = async (jwt) => {
 
 getTotal();
 
+const getCountry = async (paises) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/countries/${paises}`, {
+      method: "GET",
+      headers: {
+        "user-agent": "vscode-restclient",
+        "content-type": "application/json",
+        "accept": "application/json",
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6IkxlYW5uZSBHcmFoYW0iLCJ1c2VybmFtZSI6IkJyZXQiLCJpYXQiOjE1OTY1MDY4OTN9.KDSlP9ALDLvyy0Jfz52x8NePUejWOV_mZS6cq4-JZXs"
+      }
+    })
+    const { data } = await response.json();
+    const ctx2 = document.getElementById('Chart2')
+    let ubicacion = data.location
+    let confirmados = data.confirmed
+    let muertes = data.deaths
+    let recuperados = data.recovered
+    let activos = data.active
+    ubicationCasesChart(ctx2, ubicacion, confirmados, muertes, recuperados, activos)
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+}
 
-function totalCasesChart(ctx, paises, confirmados, muertes) {
+function totalCasesChart(ctx, ubicacion, confirmados, muertes, recuperados, activos) {
   new Chart (ctx, {
     type: 'bar',
     data: {
-      labels: paises,
+      labels: ubicacion,
       datasets: [
         {
           label: 'Confirmados',
@@ -43,11 +78,11 @@ function totalCasesChart(ctx, paises, confirmados, muertes) {
         },
         {
           label: 'Recuperados',
-          data: Math.floor((confirmados - muertes) * 0.7),
+          data: recuperados
         },
-        {
+        { 
           label: 'Activos',
-          data: Math.floor((confirmados - muertes) * 0.7),
+          data: activos,
         }
       ]
     }
@@ -66,7 +101,37 @@ function table(paises, confirmados, muertes){
         <td>${muertes}</td>
         <td>${Math.floor((confirmados - muertes) * 0.7)}</td>
         <td>${Math.floor((confirmados - muertes) * 0.3)}</td>
-        <td>modal</td>
+        <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="getCountry('${paises}')">
+        Más información
+      </button></td>
       </tr>
       `
+}
+
+
+
+
+function ubicationCasesChart(ctx2, ubicacion, confirmados, muertes, recuperados, activos) {
+  new Chart (ctx2, {
+    type: 'polarArea',
+    data: {
+      labels: [
+        'Confirmados',
+        'Muertes',
+        'Recuperados',
+        'Activos'
+      ],
+      datasets: [{
+        label: `${ubicacion}`,
+        data: [confirmados, muertes, recuperados, activos],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(75, 192, 192)',
+          'rgb(255, 205, 86)',
+          'rgb(201, 203, 207)',
+          'rgb(54, 162, 235)'
+        ]
+      }]
+    }
+  })
 }
